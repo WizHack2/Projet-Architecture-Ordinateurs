eafda951375bbc3dea8b56f0db8eaa47433c7578
WizHack <superjeremie@orange.fr>
2025-12-01 15:37:33 +0100

    Refactor minirisc_decode_and_execute to use minirisc_set_reg for register updates and implement JAL instruction handling

--- emulator/minirisc.c
+++ emulator/minirisc.c
@@ -20,6 +20,10 @@ minirisc_t* minirisc_new(uint32_t initial_PC, platform_t *platform) {
     return minirisc;
 }
 
+void minirisc_set_reg(minirisc_t* mr, int n_registre, uint32_t valeur) {
+    if (n_registre != 0) mr->regs[n_registre] = valeur;
+}
+
 void minirisc_free(minirisc_t* mr) {
     free(mr);
 }
@@ -37,14 +41,26 @@ void minirisc_fetch(minirisc_t *mr) {
 
 void minirisc_decode_and_execute(minirisc_t* mr) {
     uint32_t opcode = mr->IR & 0x7F;
+    int RD =  (mr->IR >> 7) & 0x1F; //Récupère le registre cible. On décale de 7 bits et on mets un masque pour avoir les 5 LSB de ce qu'il reste. 
 
     switch (opcode) {
         case 1: // LUI
-            int RD =  (mr->IR >> 7) & 0x1F;
-            mr->regs[RD] = mr->IR & 0xFFFFF000;
+            uint32_t newValue = (mr->IR & 0xFFFFF000) ;
+            minirisc_set_reg(mr,RD,newValue);
             break;
         case 2: //AUIPC
-            //TODO AUIPC
+            uint32_t newValue = mr->PC + (mr->IR & 0xFFFFF000);
+            minirisc_set_reg(mr,RD,newValue);
+            break;
+        case 3: //JAL
+            uint32_t offset = (mr->IR & 0x001FFFFE); // LSB de l'offset a 0. Les MSBs a 0 avant d'étendre ou non pour le signe.
+            uint32_t MSB_extensionDeSigne = (mr->IR & 0x00100000); // Pour pouvoir étendre le signe
+            if (MSB_extensionDeSigne == 0x00100000) {
+                offset += 0xFFFE0000;
+            }
+            mr->PC += offset;
+            minirisc_set_reg(mr,RD,mr->PC + 4);
+            break;
         default:
             mr->halt = 1;
             break;
@@ -73,16 +89,24 @@ void minirisc_test() {
     platformTest = platform_new();
     miniriscTest = minirisc_new(0x80000000, platformTest);
 
+    // Test écriture sur le registre x0
+    // TODOs
+
     // Test de LUI
     platform_load_program(platformTest, "/home/wizhack/Document/ensta/architectureordinateurs/embedded_software/lui_test/build/esw.bin");
     minirisc_run(miniriscTest);
     printf("Valeur dans le registre 1 en hexadecimal: %x\n",miniriscTest->regs[1]);
-    printf("Cela correspond a la valeur %u stocké dans le registre.\n",(int) miniriscTest->regs[1] >> 12);
+    printf("Cela correspond a une valeur immédiate de %u pour lui.\n",(int) miniriscTest->regs[1] >> 12);
 
-    //TODO Faire la fonction AUIPC
-    //Test de AUIPC
-    // miniriscTest->halt = 0;
-    // platform_load_program(platformTest, "");
+    // Test de AUIPC
+    miniriscTest->halt = 0;
+    miniriscTest->PC = 0x80000000;
+    platform_load_program(platformTest, "/home/wizhack/Document/ensta/architectureordinateurs/embedded_software/auipc_test/build/esw.bin");
+    minirisc_run(miniriscTest);
+    printf("Valeur dans le registre 1 en hexadecimal: %x\n",miniriscTest->regs[1]);
+    printf("Cela correspond a une valeur immédiate de %u pour auipc.\n",(int) ((miniriscTest->regs[1] - 0x80000000) >> 12));
+
+    //TODO Faire tests unitaires pour JAL.
 
     minirisc_free(miniriscTest);
     platform_free(platformTest);
